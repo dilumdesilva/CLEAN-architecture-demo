e0d07586108c08f1ffd16853b429f3f0628dd08e
“Saurabh <saurabhg557@gmail.com>
2023-09-19 19:35:32 +1200

    feat - created managers classes in presentation layer, added theme and customised some components, added fonts.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:clean_architecture_demo/presentation/resources/theme_manager.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,12 +10,24 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My TODOs'),
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text('Hello World!'),
+              ElevatedButton(
+                  onPressed: () => {}, child: const Text('Press Me')),
+            ],
+          ),
         ),
       ),
+      theme: getApplicationTheme(),
     );
   }
 }
