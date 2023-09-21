b78dcf822de8fe7bb3e45552ad7906c22f695584
Dilum De Silva <dilum@hectre.com>
2023-09-21 23:32:32 +1200

    chore: adding icons + images and registering in asset manager

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -1,4 +1,15 @@
 const String IMAGES_PATH = "assets/images";
 const String ICONS_PATH = "assets/icons";
 
-class AssetsManager {}
+class AssetsManager {
+  static CustomIcons icons = CustomIcons();
+  static CustomImages images = CustomImages();
+}
+
+class CustomIcons {
+  String pinIcon = "$ICONS_PATH/pin_icon.png";
+}
+
+class CustomImages {
+  String pinIcon = "$IMAGES_PATH/flutter_akl.png";
+}
